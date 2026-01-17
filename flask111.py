e0ebd3bb9ff7c2f63770e2ec0332a10aa65605e3
geenko1 <fkngkage@mail.ru>
2026-01-17 18:21:04 +0700

    Update flask111.py

--- flask111.py
+++ flask111.py
@@ -1,8 +1,4 @@
 import os
 import prk1
-
-# Устанавливаем переменную окружения, чтобы в prk1.py запустился сервер
 os.environ["RUN_FLASK_SERVER"] = "1"
-
-# Запускаем сервер
 prk1.app.run(debug=True)
